d5656492f85f3b93f92e44429b4162c86e35cf39
jcasadogp <juls.c99@gmail.com>
2022-12-07 23:15:19 +0100

    wroking scripts for creating directories

--- Scripts/003_create_directories.sh
+++ Scripts/003_create_directories.sh
@@ -1,50 +1,24 @@
 cd /Users/julia/bacterialGrowth_thesis/IntermediateFiles/
 
-echo "" > created_bacteria_directories.txt
-echo "" > created_experiment_directories.txt
 echo "" > line.txt
+
 
 # eliminate header of the file
 tail -n +2 ../experiments_info.txt > ../experiments_info_mod.txt
-echo $(wc -l experiments_info_mod.txt)
 
 while read -r line; do
-    echo "=========================================="
-    echo "Nueva vuelta"
+    # echo "=========================================="
 
     echo $line > line.txt
 
-    # cd ../IntermediateFiles/
     bacteria=$(awk '{print $5}' line.txt)
-    count=$(more created_bacteria_directories.txt | grep $bacteria | wc -l)
-
-    echo $bacteria
-    echo $count
-    
-    if [ $count == 0 ];
-    then
-        cd ../Data/
-        mkdir -p $bacteria
-        echo "created $bacteria"
-        cd ../IntermediateFiles/
-        echo $bacteria >> created_bacteria_directories.txt
-    fi
-
-    # cd ../IntermediateFiles/
     experiment=$(awk '{print $3}' line.txt)
-    count2=$(more created_experiment_directories.txt | grep $experiment | wc -l)
-
-    echo $experiment
-    echo $count2
     
-    if [ $count2 == 0 ];
-    then
-        cd ../Data/$bacteria
-        mkdir -p $experiment
-        echo "created $experiment"
-        cd ../../IntermediateFiles/
-        echo $experiment >> created_experiment_directories.txt
-    fi
+    cd ../Data/
+    mkdir -p $bacteria
+    cd ../Data/$bacteria
+    mkdir -p $experiment
+    cd ../../IntermediateFiles/
     
 done < ../experiments_info_mod.txt
 
@@ -52,3 +26,12 @@ cd /Users/julia/bacterialGrowth_thesis/IntermediateFiles
 rm line.txt
 # cd ../
 # rm experiments_info_mod.txt
+
+
+cd /Users/julia/bacterialGrowth_thesis/Data
+
+echo "BT:"
+ls -lt BT/ | tail -n +2 | wc -l
+
+echo "RI:"
+ls -lt RI/ | tail -n +2 | wc -l
