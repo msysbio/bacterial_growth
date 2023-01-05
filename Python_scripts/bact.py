247ae1348f53ea2c046e9f5ecf548857c2d6af47
jcasadogp <juls.c99@gmail.com>
2023-01-05 01:42:45 +0100

    python scripts

---
+++ Python_scripts/bact.py
@@ -0,0 +1,37 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--experiment_name",
+                    
+                    
+                    )
+
+parser.add_argument("--replicate",
+                    type=int
+                    
+                    )
+
+parser.add_argument("--bacteria",
+                    "-b",
+                    
+                    
+                    )
+
+parser.add_argument("--strain",
+                    "-s",
+                    
+                    )
+
+parser.add_argument("--biculture",
+                    action="store_true",
+                    
+                    )
+
+parser.add_argument("--abundance_measurement",
+                    
+                    )
+
+parser.add_argument("--units",
+                    
+                    )                    
