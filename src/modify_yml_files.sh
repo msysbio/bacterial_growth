84e37d32267eaba41028153c3a445af3250b9106
jcasadogp <juls.c99@gmail.com>
2023-03-31 20:58:01 +0200

    fixed error and remove the files option

--- src/modify_yml_files.sh
+++ src/modify_yml_files.sh
@@ -4,7 +4,6 @@ YML_FILE=$2
 sed 's/description:/## description:/' $YML_FILE_TMP | \
     sed 's/STUDY:/\n\nSTUDY:/' | \
     sed 's/EXPERIMENT:/\n\nEXPERIMENT:/' | \
-    sed 's/PERTURBATION:/\n\PERTURBATION:/' | \
-    sed 's/FILES:/\n\nFILES:/' > $YML_FILE
+    sed 's/PERTURBATION:/\n\nPERTURBATION:/' > $YML_FILE
 
 rm $YML_FILE_TMP
