a2be66e48f97a454773a64a0194db29b558f3e74
jcasadogp <juls.c99@gmail.com>
2023-04-24 18:36:52 +0200

    add local directory to the yml files

--- src/create_populate_file.py
+++ src/create_populate_file.py
@@ -1,6 +1,3 @@
-import os
-from prettytable import PrettyTable
-
 from yml_functions import createStudyYml, createExperimentYml, createPerturbationYml, createReplicatesYml
 from bash_functions import runBash
 from user_inputs import *
@@ -8,6 +5,7 @@ from constants import *
 
 def create_yml_file(args):
 
+    yml_files_dir = LOCAL_DIRECTORY+'yml_files/'
     modifyYml = PROJECT_DIRECTORY + MODIFY_YML_FILE
 
     num_studies = args.num_studies
@@ -27,13 +25,15 @@ def create_yml_file(args):
             print('\n\tERROR: You introduced NON-VALID parameters\n\tIf you want to introduce perturbations in a new study, you should also introduce the experiment info. \n\tIf you want to introduce perturbations into an existing experiment, and hence, into an existing study, you should only take the -p/--num_perturbations flag. \n\tIf you want to create a new experiment, you should also take the -e/--num_experiments flag\n')
         else:
             createStudyYml(num_experiments, num_perturbations)
-            runBash(modifyYml, [LOCAL_DIRECTORY+'yml_info_files/study_information_tmp.yml', 'yml_info_files/study_information.yml'])
+            runBash(modifyYml, [yml_files_dir+'study_information_tmp.yml', yml_files_dir+'study_information.yml'])
+            print('\n\nstudy_information.yml created!')
 
     elif num_experiments > 0:
         study_id = chooseStudy()
         
         createExperimentYml(study_id, num_experiments, num_perturbations)
-        runBash(modifyYml, [LOCAL_DIRECTORY+'yml_info_files/experiment_information_tmp.yml', 'yml_info_files/experiment_information.yml'])
+        runBash(modifyYml, [yml_files_dir+'experiment_information_tmp.yml', yml_files_dir+'experiment_information.yml'])
+        print('\n\nexperiment_information.yml created!')
 
     
     elif num_perturbations > 0:
@@ -41,7 +41,8 @@ def create_yml_file(args):
         experiment_id = chooseExperiment(study_id)
         
         createPerturbationYml(study_id, experiment_id, num_perturbations)
-        runBash(modifyYml, [LOCAL_DIRECTORY+'yml_info_files/perturbation_information_tmp.yml', 'yml_info_files/perturbation_information.yml'])
+        runBash(modifyYml, [yml_files_dir+'perturbation_information_tmp.yml', yml_files_dir+'perturbation_information.yml'])
+        print('\n\nperturbation_information.yml created!')
         
 
     else:
@@ -50,6 +51,8 @@ def create_yml_file(args):
         perturbation_id = choosePerturbation(experiment_id)
         
         createReplicatesYml(study_id, experiment_id, perturbation_id)
-        runBash(modifyYml, [LOCAL_DIRECTORY+'yml_info_files/replicates_information_tmp.yml', 'yml_info_files/replicates_information.yml'])
+        runBash(modifyYml, [yml_files_dir+'replicates_information_tmp.yml', yml_files_dir+'replicates_information.yml'])
+        print('\n\replicates_information.yml created!')
+    
     
     print('Go to yml_info_files/ and complete the created file with the information you want to introduce in the DB.\n\n')
