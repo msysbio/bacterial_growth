dad650f91936f440eccfba1cceb4399928a126c5
Haris Zafeiropoulos <haris.zafeiropoulos@kuleuven.be>
2024-05-15 15:40:24 +0200

    replace lifetime for db connection

--- pages/Upload_Data.py
+++ pages/Upload_Data.py
@@ -621,7 +621,7 @@ def tab_step3(keywords, list_taxa_id, all_strain_data,create_private_project_id,
                     help='Select all the measurement techniques used in your study to quantify bacterial growth.'
                 )
                 conn = st.connection("BacterialGrowth", type="sql")
-                df_metabolites = conn.query('SELECT * from Metabolites;', ttl=600)
+                df_metabolites = conn.query('SELECT * from Metabolites;', ttl="1d")
                 df_metabo_name = df_metabolites['metabo_name']
                 meta_col = st.multiselect(
                     'If metabolites were measure, select which ones:',
