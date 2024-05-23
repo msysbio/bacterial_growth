508a1e764bd07b13229ea2ca3b75a6235d7cbe85
Haris Zafeiropoulos <haris.zafeiropoulos@kuleuven.be>
2024-05-23 15:27:56 +0200

    check on creds

--- pages/Upload_Data.py
+++ pages/Upload_Data.py
@@ -23,11 +23,12 @@ import db_functions as db
 from parse_raw_data import save_data_to_csv
 
 
-st.info(USER, PASSWORD, HOST, DATABASE, PORT )
-
-
 # Page config
 st.set_page_config(page_title="Upload Data", page_icon="📤", layout='wide')
+
+
+st.info(USER, PASSWORD, HOST, DATABASE, PORT )
+
 
 add_logo("figs/logo_sidebar3.png", height=100)
 with open("style.css") as css:
