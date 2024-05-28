765ff156489e711301134250243ff30a204ba0b9
sofiamonsalve <sofia.monsalveduarte@student.kuleuven.be>
2024-05-28 17:14:50 +0200

    adding help 1

--- pages/Help.py
+++ pages/Help.py
@@ -11,8 +11,128 @@ st.image('figs/HelpBanner.png')
 
 st.markdown("![badge](https://img.shields.io/badge/status-under%20development-orange?style=for-the-badge)")
 
-teo_info = st.expander("**All About Bacterial Growth Data**")
-teo_info.write('hello')
+teo_info = st.expander("**Bacterial Growth Data**")
+teo_info.markdown(
+    """
+## Factors Affecting Growth
+
+Microbial growth is only possible when all essential nutrients are provided and chemical and environmental conditions are met. 
+Yet, there is a range of conditions that a species can live in. Therefore, their growth will be influenced by these factors. 
+For instance, the temperature, pH, and oxygen level are some of the environmental factors that will affect their growth. 
+On the other hand, the presence and availability of certain nutrients like carbon, nitrogen, and sulfur, will also influence their growth,
+ as well as the competition for nutrients with other microorganisms if present.
+
+### Temperature
+
+As in a chemical reaction, microorganisms have a minimum, optimal, and maximum temperature when growing. 
+As expected, growth will be maximal at an optimal temperature, and on the contrary growth will stop beyond the minimal and maximal temperatures. 
+These values vary depending on the microbial species. In the specific case of bacteria, they can be classified into three groups: mesophiles, 
+for bacteria able to grow at medium temperatures (usually between 25°C and 40°C), psychrophiles, for bacteria able to grow at low temperatures 
+(usually between 0°C and 20°C), and thermophiles, for bacteria able to grow at high temperatures (usually between 50°C and 60°C).
+
+### pH
+
+pH affects the synthesis and structure of fundamental macromolecules for growth like DNA, proteins, and lipids. 
+Thus, the acidity or basicity of the environment described by the pH also affects microbial growth. There are three main 
+classifications in the case of bacteria according to their pH preferences: neutrophiles (grow at neutral pH), acidophiles 
+(grow at very acidic pH: < 5.5), and alkaliphiles (growth at very alkaline pH: >8.5).
+
+### Oxygen Level
+
+Aerobic species will only grow in the presence of oxygen; they obtain energy through aerobic respiration and use oxygen as 
+the terminal electron acceptor. On the other hand, obligate anaerobic species will only grow when oxygen is not present; 
+they obtain energy through fermentation and use organic or inorganic compounds as a terminal electron acceptor. Moreover, facultative anaerobic
+ species will grow with or without oxygen.
+
+### Carbon, Nitrogen, Sulfur and Phosphorus Sources
+
+For microorganisms to be able to duplicate, they have to synthesize new cell components and macromolecules. 
+Carbon, nitrogen, sulfur, and phosphorus sources are fundamental, they provide the chemical elements that are necessary for 
+building cellular structures and support metabolic pathways [11]. How microbes obtain these nutrients varies between species. 
+For example, in the case of bacteria, they can obtain carbon from organic sources (chemoheterotrophs) or carbon dioxide 
+(chemoautotrophs and photoautotrophs), nitrogen can be obtained from proteins or the atmosphere (nitrogen-fixing bacteria), 
+sulfur can be obtained from proteins, sulfates, and hydrogen sulfide and phosphorus can be obtained from inorganic phosphate salts 
+or buffers.
+
+All of the environmental factors explained above will be important when setting up the right laboratory conditions that allow microbial
+ growth under in vitro conditions.
+
+## Methods of Culture
+
+In general, the methods of culture and experimental set-ups differ depending on the objective of the specific experiment but as a general rule,
+ all types of cultures should be mixed, homogenized, and in equilibrium with the gas phase. However, cultivating microorganisms in the 
+ laboratory is a much more difficult task given the difficulty to provide all the nutrients and environmental conditions needed to grow. 
+ This often makes cultivation challenging, time-consuming, and labor-intensive. There are mainly three culture or fermentation techniques 
+ widely used to grow microbial populations in vitro: batch, fed-batch, and continuous fermentation.
+
+### Batch Fermentation
+
+A batch fermentation is a closed-culture system, this means that it contains a fixed volume of nutrients or substrates in the medium. 
+After the microbial cells are inoculated into the culture medium, they will grow until all the nutrients are depleted. At the same time that
+ nutrients are consumed, metabolic byproducts and waste materials will accumulate in the culture flask. As mentioned before, some microbial 
+ organisms like bacteria cultivated in a batch reactor usually follow the four phases of growth described above: lag, exponential, stationary, 
+ and decline phase.
+
+### Fed-batch Fermentation
+
+A fed-batch fermentation is considered a modification from the common batch culture. Initially, the fed-batch fermentation operates 
+in the same way as the batch fermentation until the culture reaches the end of the exponential phase. To extend the exponential phase, the reactor 
+will be supplemented with a controlled feeding of nutrients, this feeding will extend the log phase resulting in an increased growth . 
+As in the case of batch fermentation, byproducts will accumulate.
+
+### Continuous Fermentation
+
+A continuous fermentation is characterized by the constant addition of new nutrients to the system while at the same time, the old culture 
+broth is removed. The vessel used to grow a continuous culture is called a chemostat. In a chemostat, nutrients are continuously refreshed, 
+while medium waste products and cells are continuously removed at the same time, resulting in wash-out if species do not divide fast enough. 
+Because the influx and outflow rates are constant, the volume in a continuous culture is constant as well. The maximum working volume of the 
+vessel will not limit the influx of new medium entering as in the case of the fed-batch fermentation. The influx of substrates and flow rate in 
+a chemostat can be used to control the growth rate and the number of cells as well as expanding the exponential phase. Given that in a chemostat 
+there is a constant influx and outflow of medium, a steady state (no increase or decrease in the number of cells) will be achieved when the growth 
+rate is equal to the flow rate.
+
+
+## Quantification of Microbial Cultures
+
+Quantifying the number of cells over a period of time allows a better understanding of how the specific population grows, reaches a maximum density, 
+and declines. This also allows to calculate the growth rate and with it gain insights about the growth kinetics. This information is important to 
+understand how microorganisms behave for different pH and temperature values, or nutrients available. In co-cultures, more than one microbial 
+strain is cultivated in the same medium simultaneously. This allows us to study mutualistic and antagonistic interactions between these 
+strains, as well as their dynamics. Quantification is crucial not only to assess microbial growth but also to prepare the inoculum samples. 
+Different techniques have been developed to quantify microbial cells in a sample. All of these quantify cells in different units (Table below) 
+and have different characteristics as discussed below.
+
+| Measurement techniques  | Units                                       |
+|-------------------------|---------------------------------------------|
+| Direct Microscopic Count| Cells per volume (e.g., cells/mL, cells/cm³)|
+| Colony Forming Units (CFU) | CFU/mL (liquid samples), CFU/g (solid samples)|
+| Optical Density (OD)    | OD is dimensionless                        |
+| Flow Cytometry (FC)     | cells/mL                                    |
+| 16S rRNA Seq            | Normalized read counts                      |
+
+### Direct Microscopic Count
+
+This technique involves counting directly the number of cells by placing a small volume of the sample of interest into a hemocytometer. 
+This instrument consists of a thick glass microscope slide with a rectangular grid that facilitates the counting, depending on the cell size 
+different grids can be used. Using this method provides an easy, low-cost way to count cells without the need for incubation. On the other hand, 
+it can be susceptible to human errors given the difficulties of counting under a microscope.
+
+### Colony Forming Units (CFU)
+
+The Colony Forming Units (CFU) are a direct measurement of the number of viable cells in a sample. The method consists of diluting the sample of 
+interest a given number of times, usually in a 1:10 ratio. This procedure is known as serial dilution and it helps to better count the number of 
+CFUs in the sample. After each dilution, the diluted samples will be placed and incubated in different agar plates to allow the colonies to grow. 
+Once the incubation time finishes the number of colonies present in the agar plate is counted and the number of cells is calculated depending on the specific dilution factor and volume of the original sample.
+
+
+### Optical Density (OD)
+
+To avoid counting CFUs, another technique used to quantify growth is to measure the cell’s density by using a spectrophotometer. 
+When a microbial culture is placed in a cuvette and a beam of light passes through it, the light beam will be scattered by the bacterial cells. 
+How the beam scatters depends directly on the cell density and at the same time the cell density is proportional to the number of cells. If in a 
+sample the number of microorganisms increases, so does the
+
+    """)
 
 data_info = st.expander("**Our Database**")
 data_info.write('hello')
