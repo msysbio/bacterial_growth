ff9e5dbb99a1acb0627ec8cb5afb15b21e339828
Andrew Radev <andrey.radev@gmail.com>
2024-08-18 10:10:50 +0300

    Add flask-assets for simple asset bundling

--- flask_app/main.py
+++ flask_app/main.py
@@ -1,12 +1,33 @@
 from flask import Flask
+import flask_assets
+
 import flask_app.app.views as views
 
 
 def create_app():
     app = Flask(__name__)
+    assets = flask_assets.Environment(app)
+
+    javascripts = flask_assets.Bundle(
+        'js/vendor/jquery-3.7.1.js',
+        'js/main.js',
+        filters='jsmin',
+        output='build/bundle.js'
+    )
+    assets.register('js_bundle', javascripts)
+
+    stylesheets = flask_assets.Bundle(
+        'css/reset.css',
+        'css/main.css',
+        'css/sidebar.css',
+        filters='cssmin',
+        output='build/bundle.css'
+    )
+    assets.register('css_bundle', stylesheets)
 
     app.config.update(
         DEBUG=True,
+        ASSETS_DEBUG=True,
         TEMPLATES_AUTO_RELOAD=True,
         EXPLAIN_TEMPLATE_LOADING=True,
     )
