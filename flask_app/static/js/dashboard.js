f15187b9d1a989be59819610c4c780313a59dd66
Andrew Radev <andrey.radev@gmail.com>
2024-08-28 09:57:39 +0300

    Put charts inside of detail elements, fetch on page load

--- flask_app/static/js/dashboard.js
+++ flask_app/static/js/dashboard.js
@@ -4,14 +4,31 @@ $(document).ready(function() {
   let urlParams = new URLSearchParams(window.location.search);
   let studyId = urlParams.get('studyId');
 
-  // $page.find('.chart-form').each(function() {
-  //   let $form = $(this);
-  //
-  //   console.log($form.serializeArray());
-  // });
+  $page.find('details.experiment-container').each(function(e) {
+    $details = $(this);
+
+    if ($details.find('input[type=checkbox]:checked').log().length > 0) {
+      $details.prop('open', true);
+      let $form = $details.find('form');
+      update_chart($form);
+    }
+  });
 
   $page.on('change', 'form.chart-form', function(e) {
-    let $form       = $(e.currentTarget);
+    let $form = $(e.currentTarget);
+    update_chart($form);
+  });
+
+  $page.on('click', '.clear-chart', function(e) {
+    e.preventDefault();
+
+    let $link = $(e.currentTarget);
+    let $details = $link.parents('details.experiment-container');
+    $details.find('input[type=checkbox]').prop('checked', false);
+    $details.prop('open', false);
+  });
+
+  function update_chart($form) {
     let $experiment = $form.parents('.experiment');
     let $chart      = $experiment.find('.chart');
 
@@ -26,5 +43,5 @@ $(document).ready(function() {
         $chart.html(response);
       }
     })
-  });
+  }
 });
