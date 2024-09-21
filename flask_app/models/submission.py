ce141ad20065c41548f1a3ea8ebb0eba4884b792
Andrew Radev <andrey.radev@gmail.com>
2024-09-21 16:28:46 +0200

    Fetch project info from database

--- flask_app/models/submission.py
+++ flask_app/models/submission.py
@@ -1,3 +1,7 @@
+from uuid import uuid4
+
+import sqlalchemy as sql
+
 class Submission:
     def __init__(self, data, current_step):
         self.step = current_step or 1
@@ -8,6 +12,32 @@ class Submission:
 
         self.project = data.get('project', {'name': None, 'description': None})
 
+    def update_project(self, data, db_conn):
+        self.type = data['submission_type']
+
+        if self.type == 'new_project':
+            self.project_uuid = uuid4()
+            self.study_uuid = uuid4()
+
+            self.project = {
+                'name':        data['project_name'],
+                'description': data['project_description'],
+            }
+        elif self.type == 'new_study':
+            self.project = _get_project_data(db_conn, data['project_uuid'])
+            if self.project:
+                self.project_uuid = data['project_uuid']
+
+            self.study_uuid = uuid4()
+        elif self.type == 'update_study':
+            self.project = _get_project_data(db_conn, data['project_uuid'])
+            if self.project:
+                self.project_uuid = data['project_uuid']
+
+            self.study_uuid = data['study_uuid']
+        else:
+            raise KeyError("Unknown self type: {}".format(submission_type))
+
     def _asdict(self):
         return {
             'type':         self.type,
@@ -15,3 +45,18 @@ class Submission:
             'study_uuid':   self.study_uuid,
             'project':      self.project,
         }
+
+def _get_project_data(db_conn, uuid):
+    query = """
+        SELECT
+            projectName AS name,
+            projectDescription AS description
+        FROM Project
+        WHERE projectUniqueID = :uuid
+    """
+    result = db_conn.execute(sql.text(query), {'uuid': uuid}).one_or_none()
+
+    if result is None:
+        return None
+    else:
+        return result._asdict()
--- flask_app/pages/upload.py
+++ flask_app/pages/upload.py
@@ -14,43 +14,20 @@ def upload_index_page(step=None):
         submission=submission
     )
 
-# TODO (2024-09-19) Validate project uuid in database: Render step 1 with error
-#
+
 def submit_step_1_upload():
     with get_connection() as conn:
         submission = Submission(session.get('submission', {}), current_step=1)
-        submission.type = request.form['submission_type']
+        submission.update_project(request.form, conn)
 
-        if submission.type == 'new_project':
-            if submission.project_uuid is None:
-                submission.project_uuid = uuid4()
-            if submission.study_uuid is None:
-                submission.study_uuid = uuid4()
-
-            submission.project = {
-                'name':        request.form['project_name'],
-                'description': request.form['project_description'],
-            }
-        elif submission.type == 'new_study':
-            submission.project_uuid = request.form['project_uuid']
-            if submission.study_uuid is None:
-                submission.study_uuid = uuid4()
-
-            submission.project = {
-                'name':        f"[TODO] Name of project {submission.project_uuid}",
-                'description': f"[TODO] Description of project {submission.project_uuid}",
-            }
-        elif submission.type == 'update_study':
-            submission.project_uuid = request.form['project_uuid']
-            submission.study_uuid   = request.form['study_uuid']
-
-            submission.project = {
-                'name':        f"[TODO] Name of project {submission.project_uuid}",
-                'description': f"[TODO] Description of project {submission.project_uuid}",
-            }
+        if submission.project:
+            session['submission'] = submission._asdict()
+            return redirect(url_for('upload_index_page', step=2))
         else:
-            raise KeyError("Unknown submission type: {}".format(submission_type))
+            # No project data found, show error:
 
-        session['submission'] = submission._asdict()
-
-        return redirect(url_for('upload_index_page', step=2))
+            return render_template(
+                "pages/upload/index.html",
+                submission=submission,
+                error="No project found with this UUID"
+            )
