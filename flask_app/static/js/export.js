a85d7c58e19e3b93274e611643744fc2d8fdc451
Andrew Radev <andrey.radev@gmail.com>
2024-10-24 11:04:32 +0200

    Javascript fixes and cleanup

--- flask_app/static/js/export.js
+++ flask_app/static/js/export.js
@@ -1,24 +1,26 @@
 $(document).ready(function() {
-  let $page = $('.export-page');
-  let studyId = $page.data('studyId');
+  $('.export-page').each(function() {
+    let $page   = $(this);
+    let studyId = $page.data('studyId');
 
-  let $form    = $page.find('form');
-  let $preview = $page.find('.js-preview');
+    let $form    = $page.find('form');
+    let $preview = $page.find('.js-preview');
 
-  $form.on('change', function() {
+    $form.on('change', function() {
+      updatePreview($form);
+    });
+
     updatePreview($form);
+
+    function updatePreview($form) {
+      $.ajax({
+        url: `/study/${studyId}/export/preview`,
+        dataType: 'html',
+        data: $form.serializeArray(),
+        success: function(response) {
+          $preview.html(response);
+        }
+      })
+    }
   });
-
-  updatePreview($form);
-
-  function updatePreview($form) {
-    $.ajax({
-      url: `/study/${studyId}/export/preview`,
-      dataType: 'html',
-      data: $form.serializeArray(),
-      success: function(response) {
-        $preview.html(response);
-      }
-    })
-  }
 });
