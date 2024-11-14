94a35ff989c1ac3c34e9deaddf1757e7eeb39b34
Andrew Radev <andrey.radev@gmail.com>
2024-11-14 12:40:15 +0100

    Try to fix scenario when mysql user doesn't have a password

--- flask_app/db.py
+++ flask_app/db.py
@@ -9,13 +9,18 @@ def _create_engine():
     config = get_config()
 
     username = config.pop('username', '')
-    password = config.pop('password', '')
+    password = config.pop('password', None)
     host     = config.pop('host', 'localhost')
     port     = config.pop('port', '3306')
     database = config.pop('database')
 
+    if password is None:
+        connection_string = f"mysql+pymysql://{username}@{host}:{port}/{database}"
+    else:
+        connection_string = f"mysql+pymysql://{username}:{password}@{host}:{port}/{database}"
+
     engine = sqlalchemy.create_engine(
-        f"mysql+pymysql://{username}:{password}@{host}:{port}/{database}",
+        connection_string,
         connect_args=config,
         # Set echo=True for full query logging:
         echo=False,
