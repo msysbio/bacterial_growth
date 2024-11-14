af3bf07d06e0a46f984621c6c354ab89afc9df1b
Andrew Radev <andrey.radev@gmail.com>
2024-11-14 13:09:58 +0100

    Revert "Try to fix scenario when mysql user doesn't have a password"
    This reverts commit 94a35ff989c1ac3c34e9deaddf1757e7eeb39b34.
    
    Doesn't quite seem to work, unfortunately.

--- flask_app/db.py
+++ flask_app/db.py
@@ -9,18 +9,13 @@ def _create_engine():
     config = get_config()
 
     username = config.pop('username', '')
-    password = config.pop('password', None)
+    password = config.pop('password', '')
     host     = config.pop('host', 'localhost')
     port     = config.pop('port', '3306')
     database = config.pop('database')
 
-    if password is None:
-        connection_string = f"mysql+pymysql://{username}@{host}:{port}/{database}"
-    else:
-        connection_string = f"mysql+pymysql://{username}:{password}@{host}:{port}/{database}"
-
     engine = sqlalchemy.create_engine(
-        connection_string,
+        f"mysql+pymysql://{username}:{password}@{host}:{port}/{database}",
         connect_args=config,
         # Set echo=True for full query logging:
         echo=False,
