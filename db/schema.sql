bc60bca7f93774466d6c45294693f53f1fc3538a
Andrew Radev <andrey.radev@gmail.com>
2025-02-12 17:06:10 +0100

    Add assemblyGenBankId to schema

--- db/schema.sql
+++ db/schema.sql
@@ -329,6 +329,7 @@ CREATE TABLE Strains (
   strainId int NOT NULL AUTO_INCREMENT,
   NCBId int DEFAULT NULL,
   descriptionMember text COLLATE utf8mb4_bin,
+  assemblyGenBankId varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
   PRIMARY KEY (strainId),
   KEY fk_1 (studyId),
   CONSTRAINT Strains_fk_1 FOREIGN KEY (studyId) REFERENCES Study (studyId) ON DELETE CASCADE ON UPDATE CASCADE
@@ -397,6 +398,6 @@ CREATE TABLE TechniquesPerExperiment (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-INSERT INTO MigrationVersions VALUES (1,'2024_11_11_160324_initial_schema','2025-02-05 11:02:45'),(2,'2024_11_11_164726_remove_unique_study_description_index','2025-02-05 11:02:46'),(3,'2024_11_21_115349_allow_null_medialink','2025-02-05 11:02:46'),(4,'2024_11_21_120444_fix_unique_primary_keys','2025-02-05 11:02:46'),(5,'2025_01_30_152951_fix_bioreplicates_metadata_unique_id','2025-02-05 11:02:46'),(6,'2025_02_04_134239_rename-chebi-id','2025-02-05 11:02:46'),(8,'2025_02_05_134203_make-project-and-study-uuids-unique','2025-02-05 13:46:32');
+INSERT INTO MigrationVersions VALUES (1,'2024_11_11_160324_initial_schema','2025-02-05 11:02:45'),(2,'2024_11_11_164726_remove_unique_study_description_index','2025-02-05 11:02:46'),(3,'2024_11_21_115349_allow_null_medialink','2025-02-05 11:02:46'),(4,'2024_11_21_120444_fix_unique_primary_keys','2025-02-05 11:02:46'),(5,'2025_01_30_152951_fix_bioreplicates_metadata_unique_id','2025-02-05 11:02:46'),(6,'2025_02_04_134239_rename-chebi-id','2025-02-05 11:02:46'),(8,'2025_02_05_134203_make-project-and-study-uuids-unique','2025-02-05 13:46:32'),(10,'2025_02_12_170210_add-assembly-id-to-strains','2025-02-12 17:04:45');
 
--- Dump completed on 2025-02-05 13:46:32
+-- Dump completed on 2025-02-12 17:04:45
