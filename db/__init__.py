51899afdd6fca1bc3bbec2e0a35f6ca3cedf4af5
Andrew Radev <andrey.radev@gmail.com>
2025-02-24 12:02:09 +0100

    Dump database after migrations have run

--- db/__init__.py
+++ db/__init__.py
@@ -29,6 +29,24 @@ def get_config():
     return tomllib.loads(Path('db/config.toml').read_text())[env]
 
 
+def get_cli_connection_params():
+    config = get_config()
+
+    if 'unix_socket' in config:
+        extra_params = [f'--socket={config['unix_socket']}']
+    else:
+        extra_params = ['--protocol=tcp']
+
+    return [
+        f'-h{config["host"]}',
+        f'-u{config["username"]}',
+        f'-p{config["password"]}',
+        f'--port={config["port"]}',
+        *extra_params,
+        config['database']
+    ]
+
+
 def get_connection():
     return DB.connect()
 
