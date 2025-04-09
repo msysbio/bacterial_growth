06f8ee8fa4f206e4c1a7fa1eda6e10d87265bbef
Andrew Radev <andrey.radev@gmail.com>
2025-04-09 11:49:55 +0200

    Show all recent studies on the search page by default

--- pages/search.py
+++ pages/search.py
@@ -3,12 +3,16 @@ Search for studies based on name, used metabolites, microbial strains, and
 other criteria.
 """
 
-from flask import render_template
+from flask import (
+    g,
+    render_template,
+)
 import sqlalchemy as sql
 
 from db import get_connection
 from forms.search_form import SearchForm, SearchFormClause
 import models.study_dfs as study_dfs
+from models import Study
 
 
 def search_index_page():
@@ -20,26 +24,31 @@ def search_index_page():
     if form.validate_on_submit():
         query = study_dfs.dynamical_query(form.data['clauses'])
 
-        with get_connection() as conn:
-            studyIds = [studyId for (studyId,) in conn.execute(sql.text(query))]
+        studyIds = [studyId for (studyId,) in g.db_conn.execute(sql.text(query))]
 
-            if len(studyIds) == 0:
-                message = "Couldn't find a study with these parameters."
-                return render_template("pages/search/index.html", form=form, error=message, template_clause=template_clause)
+        if len(studyIds) == 0:
+            message = "Couldn't find a study with these parameters."
+            return render_template("pages/search/index.html", form=form, error=message, template_clause=template_clause)
+    else:
+        studyIds = g.db_session.scalars(
+            sql.select(Study.studyId)
+            .order_by(Study.updatedAt.desc())
+            .limit(5)
+        ).all()
 
-            for studyId in studyIds:
-                result = study_dfs.get_general_info(studyId, conn)
+    for studyId in studyIds:
+        result = study_dfs.get_general_info(studyId, g.db_conn)
 
-                result['experiments']               = study_dfs.get_experiments(studyId, conn)
-                result['compartments']              = study_dfs.get_compartments(studyId, conn)
-                result['communities']               = study_dfs.get_communities(studyId, conn)
-                result['microbial_strains']         = study_dfs.get_microbial_strains(studyId, conn)
-                result['biological_replicates']     = study_dfs.get_biological_replicates(studyId, conn)
-                result['abundances']                = study_dfs.get_abundances(studyId, conn)
-                result['fc_counts']                 = study_dfs.get_fc_counts(studyId, conn)
-                result['metabolites_per_replicate'] = study_dfs.get_metabolites_per_replicate(studyId, conn)
+        result['experiments']               = study_dfs.get_experiments(studyId, g.db_conn)
+        result['compartments']              = study_dfs.get_compartments(studyId, g.db_conn)
+        result['communities']               = study_dfs.get_communities(studyId, g.db_conn)
+        result['microbial_strains']         = study_dfs.get_microbial_strains(studyId, g.db_conn)
+        result['biological_replicates']     = study_dfs.get_biological_replicates(studyId, g.db_conn)
+        result['abundances']                = study_dfs.get_abundances(studyId, g.db_conn)
+        result['fc_counts']                 = study_dfs.get_fc_counts(studyId, g.db_conn)
+        result['metabolites_per_replicate'] = study_dfs.get_metabolites_per_replicate(studyId, g.db_conn)
 
-                results.append(result)
+        results.append(result)
 
     return render_template(
         "pages/search/index.html",
