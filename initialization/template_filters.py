bba19600a97757fab5560ff444b90a8aebc1dde0
Andrew Radev <andrey.radev@gmail.com>
2025-04-11 12:19:11 +0200

    Combine two loops

--- initialization/template_filters.py
+++ initialization/template_filters.py
@@ -4,8 +4,11 @@ def init_template_filters(app):
     @app.template_filter('mean')
     def _mean(values, attribute=None):
         if attribute is not None:
-            values = [getattr(v, attribute) for v in values]
-            values = [v for v in values if v is not None]
+            values = [
+                getattr(v, attribute)
+                for v in values
+                if getattr(v, attribute) is not None
+            ]
 
         if values:
             return statistics.fmean(values)
@@ -15,8 +18,11 @@ def init_template_filters(app):
     @app.template_filter('std')
     def _std(values, attribute=None):
         if attribute is not None:
-            values = [getattr(v, attribute) for v in values]
-            values = [v for v in values if v is not None]
+            values = [
+                getattr(v, attribute)
+                for v in values
+                if getattr(v, attribute) is not None
+            ]
 
         if len(values) > 1:
             return statistics.stdev(values)
