2dd2108c176e4165fd7618ee1019b3aeceaa3d99
Andrew Radev <andrey.radev@gmail.com>
2025-04-14 11:25:36 +0200

    Complete time unit conversion

--- lib/conversion.py
+++ lib/conversion.py
@@ -1,11 +1,29 @@
-# TODO (2025-04-03) Test, complete
-
 def convert_time(time, source, target):
     if source == target:
         return time
-    elif source == 'h' and target == 's':
-        return round(float(time) * 3600)
-    elif source == 'm' and target == 's':
-        return round(float(time) * 60)
+
+    # Convert down to seconds:
+    if source == 's':
+        seconds = time
+    elif source == 'm':
+        seconds = round(float(time) * 60)
+    elif source == 'h':
+        seconds = round(float(time) * 3600)
+    elif source == 'd':
+        seconds = round(float(time) * 86_400)
     else:
-        raise ValueError(f"Conversion from {source} to {target} unsupported for now")
+        raise ValueError(f"Conversion from {source} to seconds unsupported")
+
+    # Convert up to what was requested
+    if target == 's':
+        result = seconds
+    elif target == 'm':
+        result = seconds / 60
+    elif target == 'h':
+        result = seconds / 3600
+    elif target == 'd':
+        result = seconds / 86_400
+    else:
+        raise ValueError(f"Conversion from seconds to {target} unsupported")
+
+    return round(result, 2)
---
+++ tests/lib/test_conversion.py
@@ -0,0 +1,41 @@
+import unittest
+
+from lib.conversion import convert_time
+
+class TestConversion(unittest.TestCase):
+    def test_time_conversion_to_the_same_unit(self):
+        for t in (1, 0.3, 100.0, 0.5):
+            for unit in ('d', 'h', 'm', 's'):
+                self.assertEqual(convert_time(t, source=unit, target=unit), t)
+
+
+    def test_time_conversion_to_seconds(self):
+        for t in (1, 13, 100.0, 0.5):
+            self.assertEqual(convert_time(t, source='m', target='s'), t * 60)
+            self.assertEqual(convert_time(t, source='h', target='s'), t * 60 * 60)
+            self.assertEqual(convert_time(t, source='d', target='s'), t * 24 * 60 * 60)
+
+    def test_time_conversion_down(self):
+        self.assertEqual(convert_time(1, source='d', target='h'), 24)
+        self.assertEqual(convert_time(1, source='h', target='m'), 60)
+        self.assertEqual(convert_time(0.5, source='h', target='m'), 30)
+
+    def test_time_conversion_up(self):
+        for t in (1, 13, 100.0, 0.5):
+            self.assertEqual(convert_time(t * 60,           target='m', source='s'), t)
+            self.assertEqual(convert_time(t * 60 * 60,      target='h', source='s'), t)
+            self.assertEqual(convert_time(t * 24 * 60 * 60, target='d', source='s'), t)
+
+    def test_time_conversion_rounding(self):
+        self.assertEqual(convert_time(3600, source='s', target='h'), 1)
+        self.assertEqual(convert_time(5400, source='s', target='h'), 1.5)
+
+        # Rounded to 2 decimals after the dot
+        self.assertEqual(convert_time(3500, source='s', target='h'), 0.97)
+
+    def test_time_conversion_unknown_units(self):
+        with self.assertRaises(ValueError): convert_time(1, source='s', target='unknown')
+        with self.assertRaises(ValueError): convert_time(1, target='s', source='unknown')
+
+if __name__ == '__main__':
+    unittest.main()
