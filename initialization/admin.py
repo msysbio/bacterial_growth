ca9dc04f56f527ce9137c8dbe716c7b409b49d13
Andrew Radev <andrey.radev@gmail.com>
2025-05-01 18:20:38 +0200

    Format some objects better in the admin

--- initialization/admin.py
+++ initialization/admin.py
@@ -1,10 +1,13 @@
 import re
+import json
 
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from sqlalchemy.orm import configure_mappers
+from markupsafe import Markup
 
-from db import get_session, FLASK_DB
+from db import FLASK_DB
+from models.orm_base import OrmBase
 from models import (
     Calculation,
     CalculationTechnique,
@@ -19,12 +22,29 @@ from models import (
 )
 
 
+def json_formatter(_view, data, _name):
+    return Markup(f"<pre>{json.dumps(data, indent=2)}</pre>")
+
+
+def record_formatter(_view, record, _name):
+    return Markup(f"""
+        <div style="width: 200px; overflow-y: auto">
+            <pre>{json.dumps(record._asdict(), indent=2)}</pre>
+        </div>
+    """)
+
+
 class AppView(ModelView):
     can_export = True
     can_view_details = True
 
     def _prettify_name(self, name):
         return re.sub(r'([a-z])([A-Z])', r'\1 \2', name).title()
+
+    column_type_formatters = {
+        dict: json_formatter,
+        OrmBase: record_formatter,
+    }
 
 
 def init_admin(app):
