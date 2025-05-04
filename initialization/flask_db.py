eab8fe11c3153b09b53f0606c6a6b0f492332a44
Andrew Radev <andrey.radev@gmail.com>
2025-05-04 15:40:43 +0200

    Fix issues with serializing decimals in JSON

--- initialization/flask_db.py
+++ initialization/flask_db.py
@@ -1,3 +1,4 @@
+import simplejson as json
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
@@ -6,6 +7,10 @@ from db import get_config_uri, FLASK_DB
 
 def init_flask_db(app):
     app.config["SQLALCHEMY_DATABASE_URI"] = get_config_uri()
+    app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
+        'json_serializer': lambda obj: json.dumps(obj, use_decimal=True),
+        'json_deserializer': lambda obj: json.loads(obj, use_decimal=True),
+    }
 
     FLASK_DB.init_app(app)
 
