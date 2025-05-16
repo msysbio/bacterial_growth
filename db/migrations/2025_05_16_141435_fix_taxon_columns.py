ed4a6505c2893354cf909ae2096ef5aaf230099e
Andrew Radev <andrey.radev@gmail.com>
2025-05-16 14:26:40 +0200

    Add auto-incrementing primary key to Taxa

---
+++ db/migrations/2025_05_16_141435_fix_taxon_columns.py
@@ -0,0 +1,33 @@
+import sqlalchemy as sql
+
+
+def up(conn):
+    query = """
+        ALTER TABLE Taxa
+        DROP PRIMARY KEY,
+        ADD id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        CHANGE tax_id ncbiId VARCHAR(50) COLLATE utf8mb4_bin NOT NULL,
+        CHANGE tax_names name VARCHAR(512) NOT NULL,
+        ADD UNIQUE INDEX Taxa_ncbiId (ncbiId)
+        ;
+    """
+    conn.execute(sql.text(query))
+
+
+def down(conn):
+    query = """
+        ALTER TABLE Taxa
+        DROP PRIMARY KEY,
+        DROP id,
+        CHANGE ncbiId tax_id VARCHAR(512) COLLATE utf8mb4_bin NOT NULL,
+        CHANGE name tax_names VARCHAR(512) COLLATE utf8mb4_bin NOT NULL,
+        ADD PRIMARY KEY (tax_id),
+        DROP KEY Taxa_ncbiId
+        ;
+    """
+    conn.execute(sql.text(query))
+
+
+if __name__ == "__main__":
+    from lib.migrate import run
+    run(__file__, up, down)
--- db/schema.sql
+++ db/schema.sql
@@ -462,9 +462,11 @@ DROP TABLE IF EXISTS Taxa;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE Taxa (
-  tax_id varchar(512) COLLATE utf8mb4_bin NOT NULL,
-  tax_names varchar(512) COLLATE utf8mb4_bin DEFAULT NULL,
-  PRIMARY KEY (tax_id)
+  ncbiId varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `name` varchar(512) COLLATE utf8mb4_bin NOT NULL,
+  id int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (id),
+  UNIQUE KEY Taxa_ncbiId (ncbiId)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -514,5 +516,6 @@ INSERT INTO MigrationVersions VALUES
 (74,'2025_05_09_194730_fix_study_metabolite_columns','2025-05-09 17:59:54'),
 (76,'2025_05_10_112933_add_compartment_id_to_measurements','2025-05-10 09:32:30'),
 (78,'2025_05_11_154801_add_fields_to_bioreplicates','2025-05-11 13:52:06'),
-(82,'2025_05_13_172421_drop_unused_tables','2025-05-13 16:10:28');
+(82,'2025_05_13_172421_drop_unused_tables','2025-05-13 16:10:28'),
+(98,'2025_05_16_141435_fix_taxon_columns','2025-05-16 12:20:31');
 
