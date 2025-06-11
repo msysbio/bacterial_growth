a6c40c45b481731a16cfd73a015dfcd80dd6b653
Andrew Radev <andrey.radev@gmail.com>
2025-06-11 11:51:14 +0200

    Extract link helpers

--- app/view/filters/urls.py
+++ app/view/filters/urls.py
@@ -1,6 +1,23 @@
+from markupsafe import Markup
+from flask import url_for
+
+
 def ncbi_url(ncbi_id):
     return f"https://www.ncbi.nlm.nih.gov/datasets/taxonomy/{ncbi_id}/"
 
 
 def chebi_url(chebi_id):
     return f"https://www.ebi.ac.uk/chebi/searchId.do?chebiId={chebi_id}"
+
+
+def external_link(url, text=None):
+    text = text or url
+
+    return Markup(f"""<a class="external" target="_blank" rel="noreferrer" href="{url}">{text}</a>""")
+
+
+def help_link(page_name, text=None):
+    text = text or page_name.replace('-', ' ').title()
+    url = url_for('help_show_page', name=page_name)
+
+    return Markup(f"""<a target="_blank" href="{url}">{text}</a>""")
--- initialization/template_filters.py
+++ initialization/template_filters.py
@@ -10,7 +10,10 @@ def init_template_filters(app):
     app.template_filter('join_tag')(lists.join_tag)
     app.template_filter('relative_time')(time.relative_time)
     app.template_filter('map_scientific')(numbers.map_scientific)
+
     app.template_filter('ncbi_url')(urls.ncbi_url)
     app.template_filter('chebi_url')(urls.chebi_url)
+    app.template_filter('external_link')(urls.external_link)
+    app.template_filter('help_link')(urls.help_link)
 
     return app
